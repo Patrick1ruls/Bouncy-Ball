ca0fe40140e3f94d1ec99addffba9b68661ee14e
Patrick1ruls <patrickvermillion555@gmail.com>
2020-03-31 22:52:12 -0500

    Update SpaceInvaders.py
    Initialize the screen

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,2 +1,22 @@
 # Space Invaders
 # Set up the screen
+import turtle
+import os
+
+# Set up the screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+
+
+
+
+
+
+
+
+
+
+delay = raw_input("Press enter to finish.")
+#turtle.mainloop()
