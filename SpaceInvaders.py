c75bee62d565a9342d72da2c7e7d05bfba7e1bc6
Patrick1ruls <patrickvermillion555@gmail.com>
2020-03-31 23:03:05 -0500

    Update SpaceInvaders.py
    Set up game border

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -3,11 +3,32 @@
 import turtle
 import os
 
+# Global variables
+X_POS = -300
+Y_POS = -300
+BORDER_SIZE = 600
+
 # Set up the screen
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Space Invaders")
 
+def draw_border(x, y, size):
+    # Draw boarder
+    border_pen = turtle.Turtle() # Initialize object
+    border_pen.speed(0)
+    border_pen.color("white")
+    border_pen.penup()
+    border_pen.setposition(x, y) # Bottom left
+    border_pen.pensize(3)
+    border_pen.pendown()
+    for side in range(4):
+        border_pen.fd(size)
+        border_pen.lt(90) # Left turn
+    border_pen.hideturtle()
+
+def main():
+    draw_border(X_POS, Y_POS, BORDER_SIZE)
 
 
 
@@ -17,6 +38,8 @@ wn.title("Space Invaders")
 
 
 
+# Main program
+main()
 
 delay = raw_input("Press enter to finish.")
 #turtle.mainloop()
